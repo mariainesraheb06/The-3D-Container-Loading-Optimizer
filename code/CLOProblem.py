3142946f573af1ac41d3786ab83733fa83d68a67
Zaioune-Ghada <ghada.zaioune@ensia.edu.dz>
2026-04-18 21:27:52 +0100

    Add Space manager class

--- code/CLOProblem.py
+++ code/CLOProblem.py
@@ -50,3 +50,72 @@ class CLOProblem :
                     Total box volume: {self.total_box_volume:.2f}
                     Container volume: {self.container_volume:.2f}
                 """
+
+class SpaceManager:
+    def init(self, container_space):
+        # Start with one big empty container
+        self.spaces = [container_space]
+
+    def find_best_placement(self, box):
+        best_score = None
+        best_space = None
+        best_dims = None
+
+        for space in self.spaces:
+            fits, dims = space.get_best_fit(box)
+
+            if fits:
+                l, w, h = dims
+
+                # Heuristic: minimize leftover edges
+                score = (space.l - l) + (space.w - w) + (space.h - h)
+
+                if best_score is None or score < best_score:
+                    best_score = score
+                    best_space = space
+                    best_dims = dims
+
+        return best_space, best_dims
+    def place_box(self, space, dims):
+        if space not in self.spaces:
+            return
+
+        # Remove used space
+        self.spaces.remove(space)
+
+        # Split into new spaces
+        new_spaces = space.split(*dims)
+
+        # Add and clean
+        self.spaces.extend(new_spaces)
+        self.clean_spaces()
+
+    def clean_spaces(self):
+        cleaned = []
+
+        for s in self.spaces:
+            # Remove zero or negative spaces
+            if s.l <= 0 or s.w <= 0 or s.h <= 0:
+                continue
+
+            # Remove contained spaces
+            if any(other != s and self.contains(other, s) for other in self.spaces):
+                continue
+
+            cleaned.append(s)
+
+        self.spaces = cleaned
+
+    def contains(self, a, b):
+        return (
+            a.x <= b.x and
+            a.y <= b.y and
+            a.z <= b.z and
+            a.x + a.l >= b.x + b.l and
+            a.y + a.w >= b.y + b.w and
+            a.z + a.h >= b.z + b.h
+        )
+
+    def print_spaces(self):
+        for s in self.spaces:
+            print(s)
